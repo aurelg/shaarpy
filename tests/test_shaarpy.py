1573b552733dedc9af8c03617ee5ddc684b71469
Aurélien Grosdidier <aurelien.grosdidier@latitude77.org>
2017-04-07 13:48:01 +0200

    Cleanup

--- tests/test_shaarpy.py
+++ tests/test_shaarpy.py
@@ -10,12 +10,8 @@ shaarli_url = "https://my.framasoft.org/u/%s/" % login
 # Post link
 url = 'http://example.org'
 tags = ['tag1', 'tag2', 'tag3']
-desc = 'weouhwge owuehgowhug wehugowuehg'
+desc = 'description'
 
 s = Shaarpy()
 s.login(login, password, shaarli_url)
 s.post_link(url, tags, desc)
-
-# def dump(soup):
-#     with open('t.html', 'w') as f:
-#         f.write(str(soup))
